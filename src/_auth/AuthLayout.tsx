214a64b54e30275b6435dc2772e3efafc8f02900
Abhishek Verma <vermaabhimanyu593@gmail.com>
2024-05-13 18:32:26 +0530

    added shadcn and created a form

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -12,9 +12,15 @@ const AuthLayout = () => {
       ):(
         // go to signup , signin form
         <>
-          <section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet/>
           </section>
+
+          <img 
+            src="/assets/images/side-img.svg"
+            alt="logo" 
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
