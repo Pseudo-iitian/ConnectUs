dc0bc4970b719c186ebfb0b9be8bb0b252d56aa7
Abhishek Verma <vermaabhimanyu593@gmail.com>
2024-05-13 12:10:27 +0530

    saving the changes

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,24 @@
-
+import { Outlet, Navigate } from "react-router-dom"
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
   return (
-    <div>AuthLayout</div>
+    // when we were going to sign-in or sign-up then it was showing the authlayout only
+    // so what we do is to show root layout i.e. home page when authenticated and show outlet i.e. signin and singup page when not authenticated
+    <>
+      {isAuthenticated? (
+        // navigate to home page
+        <Navigate to="/"/>
+      ):(
+        // go to signup , signin form
+        <>
+          <section>
+            <Outlet/>
+          </section>
+        </>
+      )}
+    </>
+
   )
 }
 
